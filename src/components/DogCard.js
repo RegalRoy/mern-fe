209e87db1dbc36cf21488b538bfa96e1d3cbed9c
RegalRoy <regalroyacruz@gmail.com>
2023-07-07 10:58:29 -0700

    created dog crud_

--- src/components/DogCard.js
+++ src/components/DogCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
-// import {Link} from 'react-router-dom';
-import '../App.css';
+import {Link} from 'react-router-dom';
+// import '../App.css';
 
 const DogCard =(props)=>{
     const dog = props.dog;
     return (
         <div className='card-container'>
+          
           <img
             src='https://images.unsplash.com/photo-1495446815901-a7297e633e8d'
             alt='Books'
@@ -13,9 +14,9 @@ const DogCard =(props)=>{
           />
           <div className='desc'>
             <h2>
-              {/* <Link to={`/show-book/${book._id}`}>{book.title}</Link> */}
+              <Link to={`/viewDog/${dog._id}`}>{dog.dogName}</Link>
             </h2>
-            <h3>{dog.dogName}</h3>
+            <h3>{dog.dogBreed}</h3>
             <p>{dog.dogBreed}</p>
           </div>
         </div>
---
+++ src/components/ShowDogDetails.js
@@ -0,0 +1,62 @@
+import React, {useState, useEffect} from 'react';
+import {Link, useParams, useNavigate} from 'react-router-dom';
+import DogService from "../services/dog.service";
+
+const ShowDogDetails = () =>{
+    const[dog, setDog]=useState({});
+    const{id} = useParams();
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        DogService.getDog(id).then((res)=>setDog(res.data))
+        .catch((err)=>console.log("error form get dog details"));
+    },{id})
+    const onDelete=(id)=>{
+        DogService.deleteDog(id).then((res)=>navigate('/user'))
+        .catch((err)=>console.log("error form get delte dogs"));
+    }
+    const onUpdate =(id)=>{
+        navigate('/update-dog/'+id)
+    }
+    return(
+        <div>
+            <table className='table table-hover table-dark'>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <td>Name</td>
+                <td>{dog.dogName}</td>
+              </tr>
+              <tr>
+                <th scope='row'>2</th>
+                <td>Age</td>
+                <td>{dog.dogAge}</td>
+              </tr>
+              <tr>
+                <th scope='row'>3</th>
+                <td>Breed</td>
+                <td>{dog.dogBreed}</td>
+              </tr>
+              <tr>
+                <th scope='row'>4</th>
+                <td>Size</td>
+                <td>{dog.dogSize}</td>
+              </tr>
+              <tr>
+                <th scope='row'>5</th>
+                <td>Temperament</td>
+                <td>{dog.dogTemperament}</td>
+              </tr>
+             
+            </tbody>
+          </table>
+          <div className='dog-actions'>
+        <button className="btn btn-outline-danger btn-lg btn-block" onClick={()=>{onDelete(dog._id)}}>Delete</button>
+        
+        <button className='btn btn-outline-info btn-lg btn-block' onClick={()=>{onUpdate(dog._id)}}>Edit</button>
+      </div>
+        </div>
+    )
+}
+
+export default ShowDogDetails;
